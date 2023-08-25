1cf09ea6b924f57a0903e34bbee01d8cac6da510
Johan Edstedt <johan.edstedt@liu.se>
2023-08-25 07:43:24 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ setup(
     name="DeDoDe",
     packages=find_packages(include= ["DeDoDe*"]),
     install_requires=open("requirements.txt", "r").read().split("\n"),
+    python_requires='>=3.9.0',
     version="0.0.1",
     author="Johan Edstedt",
 )
